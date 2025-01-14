184730819227322905e2e7b6e66cd89e5a11b33c
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-14 14:10:53 -0600

    update environment path

--- modules/sops/secrets-for-users/default.nix
+++ modules/sops/secrets-for-users/default.nix
@@ -30,7 +30,14 @@ in {
       before = ["systemd-sysusers.service"];
       environment = cfg.environment;
       unitConfig.DefaultDependencies = "no";
-      path = cfg.age.plugins;
+      path = let
+        path = config.systemd.services.sops-install-secrets.path;
+      in
+        lib.mkForce "${lib.makeBinPath path}:${lib.makeSearchPathOutput "bin" "sbin" path}";
+      systemd.services.sops-install-secrets.path = lib.lists.optionals (config.sops.environment ? PATH) (lib.pipe config.sops.environment.PATH [
+        (lib.strings.splitString ":")
+        (builtins.map (lib.strings.removeSuffix "/bin"))
+      ]);
 
       serviceConfig = {
         Type = "oneshot";
